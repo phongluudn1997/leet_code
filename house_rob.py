29652e5694be4193fc3d4091c6b0fb0d3e6eeeb5
Huynh Phong Luu <luuhuynh@me.com>
2020-03-30 23:19:35 +0700

    added house_rob

--- house_rob.py
+++ house_rob.py
@@ -27,7 +27,58 @@ class Solution:
             return max(nums[index] + max_at_i(index - 2), max_at_i(index - 1))
         return 0 if len(nums) == 0 else max_at_i(len(nums) - 1)
 
+    @classmethod
+    def top_down_memoization(cls, nums):
+        hash_table = dict()
 
-result = Solution.top_down([183, 219, 57, 193, 94, 233, 202, 154, 65, 240, 97, 234, 100, 249, 186, 66,
-                            90, 238, 168, 128, 177, 235, 50, 81, 185, 165, 217, 207, 88, 80, 112, 78, 135, 62, 228, 247, 211])
+        if len(nums) < 1:
+            return 0
+
+        def max_at(index):
+            if index in hash_table:
+                return hash_table[index]
+            elif index < 1:
+                return nums[index]
+            elif index == 1:
+                return max(nums[0], nums[1])
+            else:
+                result = max(nums[index] + max_at(index - 2), max_at(index - 1))
+                hash_table[index] = result
+                print(hash_table)
+                return result
+        return max_at(len(nums) - 1)
+
+    @classmethod
+    def bottom_up_memoization(cls, nums):
+        if len(nums) < 1:
+            return 0
+        hash_table = dict()
+
+        def max_at(i):
+            if i == 0:
+                return nums[i]
+            elif i == 1:
+                return max(nums[0], nums[1])
+            for index in range(2, i + 1):
+                result = max(nums[index] + max_at(index - 2), max_at(index - 1))
+                hash_table[index] = result
+            return hash_table[i]
+        return max_at(len(nums) - 1)
+
+    @classmethod
+    def bottom_up_two_variables(cls, nums):
+        if len(nums) < 1:
+            return 0
+
+        max_at_i_1 = nums[0]
+        max_at_i_2 = nums[1]
+
+        for i in range(2, len(nums)):
+            current = max(nums[i] + max_at_i_2, max_at_i_1)
+            max_at_i_2, max_at_i_1 = max_at_i_1, current
+
+        return current
+
+
+result = Solution.bottom_up_two_variables([1, 3, 4, 5, 6])
 print(result)

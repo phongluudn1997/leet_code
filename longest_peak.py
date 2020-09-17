59b20c8dedb6551aacfc76b7a9e7d6dcb033d9e6
Phong Luu <luuhuynh@me.com>
2020-09-17 23:28:31 +0700

    added: algo solution for longestPeak

--- longest_peak.py
+++ longest_peak.py
@@ -36,5 +36,28 @@ def find_longest_peak(array):
     return max([find_length_of_peak(peak) for peak in find_all_peaks()])
 
 
+def longestPeak(array):
+    # Solution of Algo but it's not worked!
+    longestPeakLength = 0
+    i = 1
+    while i < len(array) - 1:
+        isPeak = array[i - 1] < array[i] and array[i] > array[i + 1]
+        if not isPeak:
+            i += 1
+            continue
+        leftIndex = i - 2
+        while leftIndex >= 0 and array[leftIndex] < array[leftIndex + 1]:
+            leftIndex -= 1
+
+        rightIndex = i + 2
+        while rightIndex < len(array) and array[rightIndex] < array[rightIndex - 1]:
+            rightIndex += 1
+
+        currentPeakLength = rightIndex - leftIndex + 1
+        longestPeakLength = max(longestPeakLength, currentPeakLength)
+        i = rightIndex
+    return longestPeakLength
+
+
 if __name__ == '__main__':
-    print(find_longest_peak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]))
+    print(longestPeak([5, 4, 3, 2, 1]))

af928d80ae9f1c3737b40700e4eb50ba65e5e01c
Phong Luu <luuhuynh@me.com>
2020-09-23 00:55:08 +0700

    added: validate BST

--- btsConstruct.py
+++ btsConstruct.py
@@ -110,6 +110,14 @@ class BST:
         else:
             self.right = replacement
 
+def isValidBST(self):
+    def validBSTHelper(node, minValue, maxValue):
+        if not node.left and not node.right:
+            return True
+        else:
+            return node.value > minValue and node.value <= maxValue and validBSTHelper(node.left, minValue, node.value) and validBSTHelper(node.right, node.value, maxValue)
+    return validBSTHelper(self, float("-inf"), float("inf"))
+
 
 if __name__ == "__main__":
     bst = BST(10)

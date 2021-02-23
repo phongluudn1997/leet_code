1cf163799be0af55e77b334722fc3fb294f3f2bb
Phong Luu <luu.huynh@fiisoft.com>
2021-02-23 11:40:49 +0700

    added: memoization

--- rescursive_multiply.py
+++ rescursive_multiply.py
@@ -1,20 +1,26 @@
 """
     Find the result of mulitiply operator not using * / just + - and >> <<
+    Solution 1:
+        3*4 = 1*4 + 2*4 = 4 + 1*4 + 1*4 = 4 + 4 + 4 = 12
 """
 
 def min_product(a, b):
     bigger = a if a > b else b
     smaller = b if b < a else a
-    return min_product_helper(smaller, bigger)
+    memo = [0 for i in range(smaller + 1)]
+    return min_product_helper(smaller, bigger, memo)
 
-def min_product_helper(smaller, bigger):
+def min_product_helper(smaller, bigger, memo):
     if smaller == 0:
         return 0
     elif smaller == 1:
         return bigger
+    elif memo[smaller] > 0:
+        return memo[smaller]
     s = smaller >> 1
-    side1 = min_product_helper(s, bigger) 
-    side2 = side1 if smaller % 2 == 0 else min_product_helper(smaller - s, bigger)
-    return side1 + side2
+    side1 = memo[s] if memo[s] > 0 else min_product_helper(s, bigger, memo) 
+    side2 = side1 if smaller % 2 == 0 else min_product_helper(smaller - s, bigger, memo)
+    memo[smaller] = side1 + side2
+    return memo[smaller]
 
-print(min_product(17, 23))
+print(min_product(999, 23))

d85860a5f4be9b4871a6d239bd3229d54700a96a
Phong Luu <huynhpluu@gmail.com>
2021-02-26 20:25:18 +0700

    added: permutations

--- permutation.py
+++ permutation.py
@@ -1,3 +1,7 @@
+"""
+    Write a method to compute all permutations of a string of unique characters.
+"""
+
 def permuatation(nums):
     res = []
 
@@ -10,9 +14,6 @@ def permuatation(nums):
     return res
 
 
-print(permuatation([1, 2, 3]))
-
-
 def all_perms(elements):
     if len(elements) <= 1:
         yield elements
@@ -23,5 +24,38 @@ def all_perms(elements):
                 yield perm[:i] + elements[0:1] + perm[i:]
 
 
-for i in all_perms([1, 2, 3, 4, 5, 6]):
-    print(i)
+def get_perms(str):
+    if len(str) == 0:
+        return []
+    elif len(str) == 1:
+        return [str]
+    result = []
+    last_character = str[-1]
+    prev_perms = get_perms(str[:-1])
+    for prev_perm in prev_perms:
+        for i in range(len(prev_perm) + 1):
+            result.append(prev_perm[:i] + last_character + prev_perm[i:])
+    return result
+
+# * Cracking the coding interview
+def get_perms_cracking_code(str):
+    if not str:
+        return None
+    permutations = []
+    if not len(str):
+        permutations.append('')
+        return permutations
+    if len(str) == 1:
+        permutations.append(str)
+        return permutations
+    first_character = str[0]
+    remain_characters = str[1:]
+    words = get_perms_cracking_code(remain_characters)
+    for word in words:
+        for i in range(len(word) + 1):
+            s = word[:i] + first_character + word[i:]
+            permutations.append(s)
+    return permutations
+
+
+print(get_perms_cracking_code('123'))

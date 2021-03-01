3eab073e9706673598b833f0f853f0806a708766
Phong Luu <huynhpluu@gmail.com>
2021-03-01 09:45:04 +0700

    added: grid travel with memoization

--- grid_travel.py
+++ grid_travel.py
@@ -1,11 +1,16 @@
 def recursive(m, n):
-    ways = 0
-    if m == 1 and n == 1:
-        return 1
-    elif m == 0 or n == 0:
-        return 0
-    else:
-        ways += recursive(m-1, n) + recursive(m, n-1)
-    return ways
+    if m == 1 and n == 1: return 1
+    if m == 0 or n == 0: return 0
+    return recursive(m-1, n) + recursive(m, n-1)
 
-print(recursive(3, 2))
+def memoization(m, n, memo={}):
+    key = str(m) + ', ' + str(n)
+    if key in memo: return memo[key]
+    if m == 1 and n == 1: return 1
+    if m == 0 or n == 0: return 0
+    memo[key] = memoization(m-1, n, memo) + memoization(m, n-1, memo)
+    return memo[key]
+
+print(memoization(1, 1))
+print(memoization(1, 3))
+print(memoization(18, 18))

3b5f71474046e2cd89c9a85b60d69d9d6c8ec931
Phong Luu <huynhpluu@gmail.com>
2021-03-01 21:36:43 +0700

    added: coin_change

--- coin_change.py
+++ coin_change.py
@@ -1,16 +1,16 @@
 def coin_change(amount, coins, memo={}):
     if amount in memo: return memo[amount]
     if amount == 0: return 0
-    if amount < 0: return None
+    if amount < 0: return -1
 
-    shortest_path = None
+    shortest_path = -1
 
     for coin in coins:
         remainder = amount - coin
         remainder_combination = coin_change(remainder, coins, memo)
-        if remainder_combination is not None:
+        if remainder_combination > -1:
             path = 1 + remainder_combination
-            shortest_path = path if shortest_path is None or path < shortest_path else shortest_path
+            shortest_path = path if shortest_path == -1 or path < shortest_path else shortest_path
 
     memo[amount] = shortest_path
     return memo[amount]
@@ -18,6 +18,6 @@ def coin_change(amount, coins, memo={}):
 
 
 
-# print(coin_change(3, [1,2]))
-# print(coin_change(0, [1,2]))
-print(coin_change(5, [4,3]))
+print(coin_change(3, [1,2]))
+print(coin_change(0, [1,2]))
+print(coin_change(3, [2]))

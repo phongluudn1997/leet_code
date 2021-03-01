bb3e479e5aec7563a89c82fe9c6154c36c55333d
Phong Luu <huynhpluu@gmail.com>
2021-03-01 12:14:08 +0700

    added: memoized best_sum

--- best_sum.py
+++ best_sum.py
@@ -12,7 +12,28 @@ def best_sum(target_sum, numbers):
             shortest_path =  combination if shortest_path is None or len(combination) < len(shortest_path) else shortest_path
 
     return shortest_path
+
+
+def memoization(target_sum, numbers, memo={}):
+    if target_sum in memo: return memo[target_sum]
+    if target_sum == 0: return []
+    if target_sum < 0: return None
+
+    shortest_path = None
+    
+    for number in numbers:
+        remainder = target_sum - number
+        remainderCombination = memoization(remainder, numbers, memo)
+        if remainderCombination is not None:
+            combination = remainderCombination + [number]
+            shortest_path = combination if shortest_path is None or len(combination) < len(shortest_path) else shortest_path
+
+    memo[target_sum] = shortest_path
+    return shortest_path
             
 
-print(best_sum(7, [5, 3, 4, 7]))
-print(best_sum(8, [2, 3, 5]))
+# print(memoization(7, [5, 3, 4, 7]))
+# print(memoization(8, [2, 3, 5]))
+# print(memoization(8, [1, 4, 5]))
+# print(memoization(100, [1, 2, 5, 25]))
+print(memoization(300, [7, 14]))

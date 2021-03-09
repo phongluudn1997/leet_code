253a83cadef96867d58e361beae2030153c34daa
Phong Luu <huynhpluu@gmail.com>
2021-03-09 14:20:39 +0700

    sum: sum_overview

--- best_sum.py
+++ best_sum.py
@@ -1,39 +1,46 @@
 def best_sum(target_sum, numbers):
-    if target_sum == 0: return []
-    if target_sum < 0: return None
+    if target_sum == 0:
+        return []
+    if target_sum < 0:
+        return None
 
     shortest_path = None
-    
+
     for number in numbers:
         remainder = target_sum - number
         remainderCombination = best_sum(remainder, numbers)
         if remainderCombination is not None:
             combination = remainderCombination + [number]
-            shortest_path =  combination if shortest_path is None or len(combination) < len(shortest_path) else shortest_path
+            shortest_path = combination if shortest_path is None or len(
+                combination) < len(shortest_path) else shortest_path
 
     return shortest_path
 
 
 def memoization(target_sum, numbers, memo={}):
-    if target_sum in memo: return memo[target_sum]
-    if target_sum == 0: return []
-    if target_sum < 0: return None
+    if target_sum in memo:
+        return memo[target_sum]
+    if target_sum == 0:
+        return []
+    if target_sum < 0:
+        return None
 
     shortest_path = None
-    
+
     for number in numbers:
         remainder = target_sum - number
         remainderCombination = memoization(remainder, numbers, memo)
         if remainderCombination is not None:
             combination = remainderCombination + [number]
-            shortest_path = combination if shortest_path is None or len(combination) < len(shortest_path) else shortest_path
+            shortest_path = combination if shortest_path is None or len(
+                combination) < len(shortest_path) else shortest_path
 
     memo[target_sum] = shortest_path
     return shortest_path
-            
+
 
 # print(memoization(7, [5, 3, 4, 7]))
-# print(memoization(8, [2, 3, 5]))
+print(memoization(8, [5, 4]))
 # print(memoization(8, [1, 4, 5]))
 # print(memoization(100, [1, 2, 5, 25]))
-print(memoization(300, [7, 14]))
+# print(memoization(300, [7, 14]))
--- sum_overview.py
+++ sum_overview.py
@@ -4,7 +4,8 @@ E.g. Input: 11 and [1,2,5]
 1. Can 11 sum up by [1,2,5]
 2. Find one path to sum up 11 => [5,2,2,2]
 3. Find the length of shortest path => 3 because [5,5,1] is the shortest path
-4. Find all path
+4. Find shortest path
+5. Find all path
 """
 
 
@@ -38,4 +39,66 @@ def can_sum_up_with_memozation(total, numbers, memo={}):
     return memo[total]
 
 
-print(can_sum_up_with_memozation(111, [1]))
+def find_one_path(total, numbers):
+    if total == 0:
+        return []
+    if total < 0:
+        return None
+    for number in numbers:
+        remainder = total - number
+        result = find_one_path(remainder, numbers)
+        if result is not None:
+            return result + [number]
+
+
+def find_one_path_with_memo(total, numbers, memo={}):
+    if total == 0:
+        return []
+    if total < 0:
+        return None
+    if total in memo:
+        return memo[total]
+    for number in numbers:
+        remainder = total - number
+        result = find_one_path(remainder, numbers)
+        if result is not None:
+            memo[total] = result + [number]
+            return memo[total]
+
+
+def find_shortest_path(total, numbers):
+    if total == 0:
+        return []
+    if total < 0:
+        return None
+    shorstest_path = None
+    for number in numbers:
+        remainder = total - number
+        path_of_child = find_shortest_path(remainder, numbers)
+        if path_of_child is not None:
+            current_path = path_of_child + [number]
+            shorstest_path = current_path if shorstest_path is None or len(
+                current_path) < len(shorstest_path) else shorstest_path
+    return shorstest_path
+
+
+def find_shortest_path_with_memo(total, numbers, memo={}):
+    if total == 0:
+        return []
+    if total < 0:
+        return None
+    if total in memo:
+        return memo[total]
+    shorstest_path = None
+    for number in numbers:
+        remainder = total - number
+        path_of_child = find_shortest_path_with_memo(remainder, numbers, memo)
+        if path_of_child is not None:
+            current_path = path_of_child + [number]
+            shorstest_path = current_path if shorstest_path is None or len(
+                current_path) < len(shorstest_path) else shorstest_path
+    memo[total] = shorstest_path
+    return shorstest_path
+
+
+print(find_shortest_path_with_memo(1341, [5, 2, 1]))

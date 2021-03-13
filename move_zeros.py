c0fc0a53f44967b66afb101daaf6be05dedec24d
Phong Luu <huynhpluu@gmail.com>
2021-03-13 17:57:09 +0700

    added: move zero

--- move_zeros.py
+++ move_zeros.py
@@ -1,11 +1,20 @@
+"""
+https://leetcode.com/problems/move-zeroes/discuss/172432/THE-EASIEST-but-UNUSUAL-snowball-JAVA-solution-BEATS-100-(O(n))-%2B-clear-explanation
+"""
+
+
 class Solution:
     def move_zeroes(self, nums):
-        for i in nums:
-            if i == 0:
-                nums.remove(i)
-                nums.append(0)
-                print(nums)
+        size = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                size += 1
+            else:
+                # swap with the most left 0
+                nums[i-size], nums[i] = nums[i], nums[i-size]
+        return nums
 
 
 solution = Solution()
-solution.move_zeroes([1, 2, 3, 4, 0, 2, 3, 0, 1, 2])
+result = solution.move_zeroes([1, 2, 3, 4, 0, 2, 3, 0, 1, 2])
+print(result)
